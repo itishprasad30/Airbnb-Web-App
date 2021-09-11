974f4a21cb161be52d81f865328343dc4658717a
itishprasad30 <itishprasad20@gmail.com>
2021-09-11 08:14:56 +0530

    loading animation is done🤟

--- pages/_app.js
+++ pages/_app.js
@@ -2,23 +2,17 @@ import "tailwindcss/tailwind.css";
 import "../styles/global.css";
 import ProgressBar from "@badrap/bar-of-progress";
 import Router from "next/router";
-const progress = new ProgressBar({
+
+const progressBar = new ProgressBar({
   size: 4,
-
-  // Color of the progress bar.
-  // Also used for the glow around the bar.
-  color: "#59FE59",
-
-  // Class name used for the progress bar element.
+  color: "#F06211",
   className: "z-50",
-
-  // How many milliseconds to wait before the progress bar
-  // animation starts after calling .start().
   delay: 100,
 });
-Router.events.on("routeChangeStart", progress.start);
-Router.events.on("routeChangeEnd", progress.finish);
-Router.events.on("routeChangeError", progress.finish);
+
+Router.events.on("routeChangeStart", progressBar.start);
+Router.events.on("routeChangeComplete", progressBar.finish);
+Router.events.on("routeChangeError", progressBar.finish);
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
